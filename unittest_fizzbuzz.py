34e0ce36f7c2d7f3b86f20f3dbff8b7ddd7ba25a
Timothy Nguyen <timothynguyen2000@yahoo.com>
2021-05-27 23:50:47 -0700

    Wrote 2nd test, input '55' should print 'Buzz'

--- unittest_fizzbuzz.py
+++ unittest_fizzbuzz.py
@@ -4,4 +4,6 @@ import fizzbuzz
 class TestBuzz(unittest.TestCase):
     def test_fizz(self):
         self.assertEqual(fizzbuzz.fuzz(33),"Fizz")
-	
+        
+    def test_buzz(self):
+        self.assertEqual(fizzbuzz.fuzz(55),"Buzz")
