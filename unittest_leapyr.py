470241fd32257cc4d0a65f0b0890203b8d7a3816
Timothy Nguyen <timothynguyen2000@yahoo.com>
2021-05-28 09:58:44 -0700

    Wrote 2nd test, input '200' should return False

--- unittest_leapyr.py
+++ unittest_leapyr.py
@@ -4,3 +4,6 @@ import leapyr
 class Testleapyr(unittest.TestCase):
     def test_div4(self):
         self.assertEqual(leapyr.isleap(24),True)
+    def test_div100(self):
+        self.assertEqual(leapyr.isleap(200),False)
+   
